804d6c12d2d18f5fc0859e0d09fd068b8b2c3cd5
jruizsilva <ruizsilvajonathan@gmail.com>
2024-01-25 19:54:35 -0300

    routes added

--- landing-page-angular/src/app/product-detail/product-detail.component.ts
+++ landing-page-angular/src/app/product-detail/product-detail.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './product-detail.component.html',
   styles: ``,
 })
-export class ProductDetailComponent {}
+export class ProductDetailComponent implements OnInit {
+  _route = inject(ActivatedRoute);
+  product = '';
+  color = '';
+
+  ngOnInit(): void {
+    this._route.params.subscribe((params) => {
+      this.product = params['productId'];
+      this.color = params['category'];
+    });
+  }
+}
